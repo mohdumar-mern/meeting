5f05b892b2e9fe4fa189efcbae31386beaf109ac
Mohd88umar <mohd.umar@trayistats.com>
2025-07-22 22:52:05 +0530

    added to api-url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
   reducerPath: 'api', 
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_APP_URL ||  'http://localhost:8080/api',
+    baseUrl: import.meta.env.VITE_APP_URL ,
     credentials: 'include', 
   }),
   tagTypes: ['Meeting'], 
